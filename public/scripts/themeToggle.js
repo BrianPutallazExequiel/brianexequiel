f810f1f421b7868b7ffa45553d9753e7cefc0c6e
Brian Exequiel Putallaz <brianexequielputallaz@gmail.com>
2025-03-08 04:17:53 -0300

    fix: :sparkles: Add theme switch

---
+++ public/scripts/themeToggle.js
@@ -0,0 +1,12 @@
+const themeToggle = document.getElementById('theme-toggle');
+
+const toggleTheme = () => {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    
+    document.documentElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+};
+
+// Inicialización
+themeToggle.addEventListener('click', toggleTheme);
