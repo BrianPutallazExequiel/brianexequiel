f00aabe894f85199307d54f6de99dbb7d2a3f749
Brian Exequiel Putallaz <brianexequielputallaz@gmail.com>
2025-03-09 04:44:09 -0300

    Revert "feat: :sparkles: Add i18n"
    This reverts commit 2b7bb7c160c3bd12dc0744aad2fd702740ca3a7a.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,5 @@ import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 
 export default defineConfig({
-  integrations: [image()],
-  i18n: {
-    defaultLocale: 'es',
-    locales: ['es', 'en'],
-    routing: {
-      prefixDefaultLocale: true
-    }
-  },
+  integrations: [image()] // <-- Debe estar añadido
 });
