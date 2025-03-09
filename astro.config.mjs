2b7bb7c160c3bd12dc0744aad2fd702740ca3a7a
Brian Exequiel Putallaz <brianexequielputallaz@gmail.com>
2025-03-09 00:01:38 -0300

    feat: :sparkles: Add i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,12 @@ import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 
 export default defineConfig({
-  integrations: [image()] // <-- Debe estar añadido
+  integrations: [image()],
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: true
+    }
+  },
 });
