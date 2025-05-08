9c13598f76a65a36c3bf219a642f10328fe9c8db
Brian Putallaz <brianexequielputallaz@gmail.com>
2025-05-07 21:05:46 -0300

    feat: :globe_with_meridians: Add translations for hero section texts

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -11,13 +11,21 @@ export const ui = {
         'projects': 'Proyectos',
         'blog': 'Blog',
         'contact': 'Contacto',
-        'contact.button': 'Contactame'
+        'contact.button': 'Contactame',
+        'title.presentation': '👋 Soy Brian Exequiel',
+        'title.profesion': 'Desarrollador Web',
+        'description.hero': 'Transformo ideas en negocios digitales únicos, integrando desarrollo, diseño y marketing para crear experiencias que destacan.',
+        'download.button': 'Descargar CV'
     },
     en: {
         'about': 'About me',
         'projects': 'Projects',
         'contact': 'Contact',
         'blog': 'Blog',
-        'contact.button': 'Contact me'
+        'contact.button': 'Contact me',
+        'title.presentation': '👋 I´m Brian Exequiel',
+        'title.profesion': 'Web Developer',
+        'description.hero': 'I turn ideas into unique digital businesses, integrating development, design, and marketing to create standout experiences.',
+        'download.button': 'Download CV'
     },
 } as const;
