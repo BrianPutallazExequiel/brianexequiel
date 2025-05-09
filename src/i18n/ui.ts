cba9e4dc5d5bcac0a0a2cb46915ba655f9eb0fcf
Brian Putallaz <brianexequielputallaz@gmail.com>
2025-05-08 22:06:10 -0300

    feat: :sparkles: Add responsive About.astro section to project

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -15,7 +15,13 @@ export const ui = {
         'title.presentation': '👋 Soy Brian Exequiel',
         'title.profesion': 'Desarrollador Web',
         'description.hero': 'Transformo ideas en negocios digitales únicos, integrando desarrollo, diseño y marketing para crear experiencias que destacan.',
-        'download.button': 'Descargar CV'
+        'download.button': 'Descargar CV',
+        'title.slider': 'Conocimientos avalados por:',
+        'about.title': 'Sobre mi',
+        'about.paragraph': "Soy Brian, tengo 25 años y me especializo en desarrollo Front End. Actualmente, estoy realizando un curso un intensivo de Full Stack y habilidades para la empleabilidad, tras ser seleccionado entre 600 postulantes para una beca de Fundación Banco Nación y Fundación Pescar.",
+        'expertise.text': 'Tengo conocimientos en:',
+        'expertise.text2': 'Estudiando:',
+        'about.button': 'Trabajemos juntos:'
     },
     en: {
         'about': 'About me',
@@ -26,6 +32,12 @@ export const ui = {
         'title.presentation': '👋 I´m Brian Exequiel',
         'title.profesion': 'Web Developer',
         'description.hero': 'I turn ideas into unique digital businesses, integrating development, design, and marketing to create standout experiences.',
-        'download.button': 'Download CV'
+        'download.button': 'Download CV',
+        'title.slider': 'Skills certified by:',
+        'about.title': 'About me',
+        'about.paragraph': "I'm Brian, I'm 25 years old and I specialize in Front-End development. Currently, I'm taking an intensive Full Stack and employability skills course, after being selected among 600 applicants for a scholarship from Fundación Banco Nación and Fundación Pescar.",
+        'expertise.text': 'I have knowledge in:',
+        'expertise.text2': 'Learning:',
+        'about.button': "Let's work together:"
     },
 } as const;
