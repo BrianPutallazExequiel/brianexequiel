749ec3aff2e9c2c78eb13ad626f48fce10d31c9f
Brian Putallaz <brianexequielputallaz@gmail.com>
2025-05-09 16:31:18 -0300

    feat: :sparkles: Add projects.astro component

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -21,7 +21,15 @@ export const ui = {
         'about.paragraph': "Soy Brian, tengo 25 años y me especializo en desarrollo Front End. Actualmente, estoy realizando un curso un intensivo de Full Stack y habilidades para la empleabilidad, tras ser seleccionado entre 600 postulantes para una beca de Fundación Banco Nación y Fundación Pescar.",
         'expertise.text': 'Tengo conocimientos en:',
         'expertise.text2': 'Estudiando:',
-        'about.button': 'Trabajemos juntos:'
+        'about.button': 'Trabajemos juntos:',
+        'project.title': 'Proyectos',
+        'project.subtitle': 'Más que código soluciones reales',
+        'project.description-01': 'Landing page desarrollada para Solwates, una empresa de software. Presenta sus servicios, propuesta de valor y medios de contacto mediante un diseño moderno, claro y totalmente responsive.',
+        'project.description-02': 'Landing page desarrollada para una empresa chilena dedicada a la organización y decoración de eventos. El sitio comunica su estilo, servicios y trabajos realizados mediante una interfaz atractiva, visual y adaptable a todos los dispositivos.',
+        'project.description-03': 'Landing page diseñada para Aerofly, una empresa dedicada a la venta de pasajes aéreos a destinos internacionales. El sitio ofrece una experiencia intuitiva para descubrir vuelos, promociones y gestionar reservas de forma ágil desde cualquier dispositivo.',
+        'project.technologies': 'Técnologias utilizadas:',
+        'project.button.web': 'Ir al sitio Web',
+        'project.button.github': 'Ver en Github'
     },
     en: {
         'about': 'About me',
@@ -38,6 +46,14 @@ export const ui = {
         'about.paragraph': "I'm Brian, I'm 25 years old and I specialize in Front-End development. Currently, I'm taking an intensive Full Stack and employability skills course, after being selected among 600 applicants for a scholarship from Fundación Banco Nación and Fundación Pescar.",
         'expertise.text': 'I have knowledge in:',
         'expertise.text2': 'Learning:',
-        'about.button': "Let's work together:"
+        'about.button': "Let's work together:",
+        'project.title': 'Projects',
+        'project.subtitle': 'More than code, real solutions',
+        'project.description-01': 'Landing page developed for Solwates, a software company. It showcases their services, value proposition, and contact options through a modern, clean, and fully responsive design.',
+        'project.description-02': 'Landing page developed for a Chilean company dedicated to event planning and decoration. The site showcases their style, services, and past work through an attractive, visual, and fully responsive interface.',
+        'project.description-03': 'Landing page designed for Aerofly, a company dedicated to selling airline tickets to international destinations. The site offers an intuitive experience to discover flights, promotions, and manage bookings quickly from any device.',
+        'project.technologies': 'Technologies used:',
+        'project.button.web': 'Go to the website',
+        'project.button.github': 'View on Github'
     },
 } as const;
